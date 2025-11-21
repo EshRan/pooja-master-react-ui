bd6404f2ed9b9352ff9f8eed66dd30a80ef14e66
charanteja <pasagadugulacharanteja@gmail.com>
2025-11-21 16:35:28 +0530

    adding some setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      className="flex-1 items-center justify-center"
-    >
-      <Text className="text-">Welcome</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl font-bold">Welcome Divinova</Text>
     </View>
   );
 }
-
